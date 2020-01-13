4a72e43eba23c9fabc29832412cf98032e99577b
Catalina-ui <58471544+Catalina-ui@users.noreply.github.com>
2020-01-13 20:19:11 +0900

    Create Atcoder_ABC142_C.cpp

---
+++ Atcoder_ABC142_C.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    for (int i = 0; i < N;i++)
+        cin >> A[i];
+    vector<int> ans(N,0);
+
+    for (int i = 0; i < N;i++)
+        ans[A[i]-1] = i+1;
+
+    for (int i = 0; i < N;i++)
+        cout << ans[i] << " ";
+    return 0;
+}
