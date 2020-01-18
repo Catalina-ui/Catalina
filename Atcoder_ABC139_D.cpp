aa93378dc25073d2aaa23e2ee916974608db8e93
Catalina-ui <58471544+Catalina-ui@users.noreply.github.com>
2020-01-18 23:00:52 +0900

    Create Atcoder_ABC139_D.cpp

---
+++ Atcoder_ABC139_D.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long N;
+    cin >> N;
+    long long ans = 0;
+    ans = (N - 1) * N / 2;
+    cout << ans << endl;
+    return 0;
+}
